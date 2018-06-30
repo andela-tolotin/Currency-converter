2148b87f4d6cf41d34c56c0b8b807ccfc1083a74
Temitope Olotin <temitope.olotin@andela.com>
2018-07-01 00:23:32 +0100

    beaufify the code

--- public/serviceWorker.js
+++ public/serviceWorker.js
@@ -2,70 +2,69 @@ const cacheName = 'pwa-currency-converter';
 const cacheVersion = `${cacheName}::1.0.0`;
 
 const cachedFiles = [
-    '/',
-    '/css/styles.css',
-    'https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css',
-    'js/idb.js',
-    'js/converter.js'
+  '/',
+  '/css/styles.css',
+  'https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css',
+  'js/idb.js',
+  'js/converter.js'
 ];
 
-const networkFiles = [
-];
+const networkFiles = [];
 
 self.addEventListener('install', event => {
 
-    console.log('[pwa install]');
+  console.log('[pwa install]');
 
-    event.waitUntil(
-        caches.open(cacheVersion)
-            .then(cache => cache.addAll(cachedFiles))
-    );
+  event.waitUntil(
+    caches.open(cacheVersion)
+    .then(cache => cache.addAll(cachedFiles))
+  );
 
 });
 
 self.addEventListener('activate', event => {
 
-    console.log('[pwa activate]');
+  console.log('[pwa activate]');
 
-    event.waitUntil(
-        caches.keys().then(keys =>
-            Promise.all(
-                keys.filter(key => key.indexOf(cacheName) === 0 && key !== cacheVersion)
-                    .map(key => caches.delete(key))
-            )
-        )
-    );
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.filter(key => key.indexOf(cacheName) === 0 && key !== cacheVersion)
+        .map(key => caches.delete(key))
+      )
+    )
+  );
 
-    return self.clients.claim();
+  return self.clients.claim();
 
 });
 
 self.addEventListener('fetch', event => {
 
-    if (networkFiles.filter(item => event.request.url.match(item)).length) {
+  if (networkFiles.filter(item => event.request.url.match(item)).length) {
 
-        console.log('[network fetch]', event.request.url);
+    console.log('[network fetch]', event.request.url);
 
-        event.respondWith(
-            caches.match(event.request)
-                .then(response => response || fetch(event.request))
-        );
+    event.respondWith(
+      caches.match(event.request)
+      .then(response => response || fetch(event.request))
+    );
 
-    } else {
+  } else {
 
-        console.log('[pwa fetch]', event.request.url);
+    console.log('[pwa fetch]', event.request.url);
 
-        event.respondWith(
-            caches.match(event.request)
-                .then(response => {
+    event.respondWith(
+      caches.match(event.request)
+      .then(response => {
 
-                    caches.open(cacheVersion).then(cache => cache.add(event.request.url));
+        caches.open(cacheVersion).then(cache => cache.add(event.request.url));
 
-                    return response || fetch(event.request);
+        return response || fetch(event.request);
 
-                })
-        );
+      })
+    );
 
-    }
+  }
 
 });
